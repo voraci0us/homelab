f0830dbce0aca31bf42cee2bb999792fa7cb166e
voraci0us <harrisonrkeating@gmail.com>
2026-04-27 02:34:33 -0700

    add fluxcd/git binaries

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -67,6 +67,8 @@
   environment.systemPackages = with pkgs; [
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+     fluxcd
+     git
      kubernetes-helm
      gnupg
      sqlite
